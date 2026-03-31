ad7dd227d2f19141642ade48a2c67f5075f14252
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-03-31 14:50:36 +0300

    treatment modal styles

--- src/shared/components/Button/index.tsx
+++ src/shared/components/Button/index.tsx
@@ -10,13 +10,21 @@ type ButtonProps = {
 export const Button = ({ children, type = "primary" }: ButtonProps) => {
   const decor = type === "primary" ? styles.primary : styles.secondary;
 
-  return (
+  //TODO: сделать чтобы стрелки были равны высоте текста (обернуть в общий контейнер)
+  // TODO: убрать условие в return
+
+  return type === "primary" ? (
     <button type="button" className={`${styles.button} ${decor} `}>
       <div className={styles.arrowsContainer}>
         <ArrowSVG className={styles.arrow} />
         <ArrowSVG className={styles.arrow} />
         <ArrowSVG className={styles.arrow} />
       </div>
+      <FingerprintSVG className={styles.fingerprint} />
+      <span className={styles.text}>{children}</span>
+    </button>
+  ) : (
+    <button type="button" className={`${styles.button} ${decor} `}>
       <FingerprintSVG className={styles.fingerprint} />
       <span className={styles.text}>{children}</span>
     </button>
--- src/shared/components/ModalTreatment/index.tsx
+++ src/shared/components/ModalTreatment/index.tsx
@@ -1,18 +1,29 @@
 import { Button } from "../Button";
+import { ArrowSVG } from "@/shared/assets/svg/Arrow";
 import styles from "./ModalTreatment.module.css";
 
 const ModalTreatment = () => {
   return (
     <div className={styles.modalTreatment}>
       <img className={styles.image} src="/mockImage.jpg" alt="фотка заглушка" />
-      <div className={styles.descriptionWrapper}>
-        <h2 className={styles.name}>Название процедуры</h2>
-        <p className={styles.description}>
-          Целенаправленная процедура для глубокой очистки пор, снижения
-          воспалений и профилактики высыпаний. Включает мягкое отшелушивание,
-          распаривание, ручную чистку (удаление комедонов) и успокаивающий
-          завершающий этап.
-        </p>
+      <div className={styles.info}>
+        <h2 className={styles.treatmentName}>Название процедуры</h2>
+        <div className={styles.descriptionWrapper}>
+          <p className={styles.description}>
+            Целенаправленная процедура для глубокой очистки пор, снижения
+            воспалений и профилактики высыпаний. Включает мягкое отшелушивание,
+            распаривание, ручную чистку (удаление комедонов) и успокаивающий
+            завершающий этап.
+          </p>
+          <div className={styles.infButton}>
+            <a className={styles.buttonText} type="button">
+              Узнать подробнее
+            </a>
+            {/*TODO: ЗАМЕНИТЬ текущую анимацию на движущуюся стрелку или смену opacity каждой буквы*/}
+            <img className={styles.arrow} src="/arrow.svg" alt="icon" />
+          </div>
+        </div>
+
         <Button type="secondary">Запись</Button>
       </div>
     </div>

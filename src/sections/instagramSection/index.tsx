a9cbf565beef1a6981b03e8af5f8e014c935328f
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-04-23 03:06:23 +0300

    starting to add a video-carousel

--- src/sections/instagramSection/index.tsx
+++ src/sections/instagramSection/index.tsx
@@ -1,15 +1,45 @@
-import { Suspense } from "react";
+"use client";
+
+import { SyntheticEvent, useRef } from "react";
 import styles from "./InstagramSection.module.css";
-import { Posts } from "./Posts";
 
 export function InstagramSection() {
+  const progressBar = useRef<HTMLDivElement | null>(null);
+  const video = useRef<HTMLVideoElement | null>(null);
+
+  function updateProgress() {
+    const vid = video.current;
+    const bar = progressBar.current;
+    if (!vid || !bar) return;
+    if (!vid.paused && !vid.ended) {
+      const percentage = -100 + (vid.currentTime / vid.duration) * 100;
+      bar.style.transform = `translateX(${percentage}%)`;
+      requestAnimationFrame(updateProgress);
+    }
+  }
+
+  function onTimeUpdate(e: SyntheticEvent<HTMLVideoElement>) {
+    requestAnimationFrame(updateProgress);
+  }
+
   return (
     <section className={styles.sectionContainer}>
       <h2 className={styles.heading}>Остаёмся на связи</h2>
       <div className={styles.gallery}>
-        <Suspense fallback={<div className={styles.fallback}>Загрузка</div>}>
-          <Posts />
-        </Suspense>
+        <div className={styles.videoWrapper}>
+          <video
+            ref={video}
+            onPlay={onTimeUpdate}
+            id="video"
+            controlsList="play notimeline volume"
+            controls
+            className={styles.video}
+            src="/video/guinot.mp4"
+          />
+          <div className={styles.progressBarContainer}>
+            <div ref={progressBar} className={styles.progressBar} />
+          </div>
+        </div>
       </div>
     </section>
   );

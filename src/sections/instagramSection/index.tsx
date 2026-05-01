ae609967e25e31ffb790463edbf0bba79e8c1983
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-05-02 00:03:21 +0300

    carousel improvement

--- src/sections/instagramSection/index.tsx
+++ src/sections/instagramSection/index.tsx
@@ -1,69 +1,98 @@
 "use client";
 
-import { SyntheticEvent, useRef } from "react";
+import {
+  SyntheticEvent,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./InstagramSection.module.css";
 import { ArrowSVG } from "@/shared/assets/svg/Arrow";
 
-export function InstagramSection() {
+import { MOCKDATA } from "./mockData";
+import clsx from "clsx";
+
+export const InstagramSection = () => {
   const progressBar = useRef<HTMLDivElement | null>(null);
-  const video = useRef<HTMLVideoElement | null>(null);
+  // const video = useRef<HTMLVideoElement | null>(null);
   const videoContainer = useRef<HTMLDivElement | null>(null);
 
-  function updateProgress() {
-    const vid = video.current;
-    const bar = progressBar.current;
-    if (!vid || !bar) return;
-    if (!vid.paused && !vid.ended) {
-      const percentage = -100 + (vid.currentTime / vid.duration) * 100;
-      bar.style.transform = `translateX(${percentage}%)`;
-      requestAnimationFrame(updateProgress);
-    }
-  }
+  // function updateProgress() {
+  //   const vid = video.current;
+  //   const bar = progressBar.current;
+  //   if (!vid || !bar) return;
+  //   if (!vid.paused && !vid.ended) {
+  //     const percentage = -100 + (vid.currentTime / vid.duration) * 100;
+  //     bar.style.transform = `translateX(${percentage}%)`;
+  //     requestAnimationFrame(updateProgress);
+  //   }
+  // }
 
-  function onTimeUpdate(e: SyntheticEvent<HTMLVideoElement>) {
-    requestAnimationFrame(updateProgress);
-  }
+  // function onTimeUpdate(e: SyntheticEvent<HTMLVideoElement>) {
+  //   requestAnimationFrame(updateProgress);
+  // }
+
+  const [activeIndex, setActiveIndex] = useState(
+    Math.floor((MOCKDATA.length - 1) / 2),
+  );
+
+  useLayoutEffect(() => {
+    const container = videoContainer.current;
+    const activeVideo = container?.children?.[activeIndex] as HTMLVideoElement;
+
+    if (!container || !activeVideo || !container.parentElement) return;
+
+    const viewportWidth = container.parentElement.offsetWidth;
+
+    // 2. Находим центр активного видео относительно начала трека
+    // (индекс * (ширина + отступ)) + половина ширины видео
+    const gap = 30;
+    const elementCenterInTrack =
+      activeIndex * (activeVideo.offsetWidth + gap) +
+      activeVideo.offsetWidth / 2;
+
+    // 3. Вычисляем смещение: центр экрана минус центр элемента
+    const translateX = viewportWidth / 2 - elementCenterInTrack;
+
+    container.style.transform = `translateX(${translateX}px)`;
+  }, [activeIndex]);
+
+  useEffect(() => {
+    videoContainer.current?.classList.add(`${styles.animated}`);
+  }, [activeIndex]);
 
   return (
     <section className={styles.sectionContainer}>
       <h2 className={styles.heading}>Остаёмся на связи</h2>
-      <div ref={videoContainer} className={styles.videoContainer}>
+
+      <div className={styles.carousel}>
         <button
           onClick={() => {
-            videoContainer.current?.scrollBy({
-              left: -300,
-              behavior: "smooth",
-            });
+            setActiveIndex((prev) => prev - 1);
           }}
           className={`${styles.button} ${styles.backButton}`}
         >
           <ArrowSVG className={styles.arrow} />
         </button>
-
-        <video src="/video/pacan.mp4" className={styles.video} />
-        <video
-          ref={video}
-          onPlay={onTimeUpdate}
-          className={styles.video}
-          src="/video/guinot.mp4"
-        />
-        <video src="/video/jetpeel.mp4" className={`${styles.video}`} />
-        <video src="/video/outside.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-
-        <video src="/video/alena.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-        <video src="/video/alena.mp4" className={styles.video} />
-
+        <div ref={videoContainer} className={styles.videoContainer}>
+          {MOCKDATA.map((link, index) => {
+            return (
+              <video
+                key={index}
+                // ref={video}
+                // onPlay={onTimeUpdate}
+                className={clsx(`${styles.video}`, {
+                  [`${styles.activeVideo}`]: index === activeIndex,
+                })}
+                src={link}
+              />
+            );
+          })}
+        </div>
         <button
           onClick={() => {
-            videoContainer.current?.scrollBy({
-              left: 300,
-              behavior: "smooth",
-            });
+            setActiveIndex((prev) => prev + 1);
           }}
           className={`${styles.button} ${styles.forwardButton}`}
         >
@@ -90,4 +119,4 @@ export function InstagramSection() {
       </div>
     </section>
   );
-}
+};
---
+++ src/sections/instagramSection/mockData.ts
@@ -0,0 +1,10 @@
+export const MOCKDATA: string[] = [
+  "/video/guinot.mp4",
+  "/video/jetpeel.mp4",
+  "/video/outside.mp4",
+  "/video/alena.mp4",
+  "/video/electro-woman.mp4",
+  "/video/pacan.mp4",
+  "/video/vita1.mp4",
+  "/video/vita2.mp4",
+];

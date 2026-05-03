f4dbeb2985cde80c39564428825bcb79eb748a3f
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-05-03 15:18:19 +0300

    carousel appearance animation

--- src/sections/instagramSection/index.tsx
+++ src/sections/instagramSection/index.tsx
@@ -1,24 +1,22 @@
 "use client";
 
-import {
-  SyntheticEvent,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./InstagramSection.module.css";
 import { ArrowSVG } from "@/shared/assets/svg/Arrow";
 
 import { MOCKDATA } from "./mockData";
 import clsx from "clsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 export const InstagramSection = () => {
   const videoContainer = useRef<HTMLDivElement | null>(null);
-
   const [activeIndex, setActiveIndex] = useState(
     Math.floor((MOCKDATA.length - 1) / 2),
   );
+
+  const track = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
     const container = videoContainer.current;
@@ -35,12 +33,44 @@ export const InstagramSection = () => {
     // Вычисляем смещение
     const translateX = viewportWidth / 2 - elementCenterInTrack;
 
-    container.style.transform = `translateX(${translateX}px)`;
+    container.style.transform = `matrix(1, 0, 0, 1, ${translateX}, 0)`;
   }, [activeIndex]);
 
-  useEffect(() => {
-    videoContainer.current?.classList.add(`${styles.animated}`);
-  }, [activeIndex]);
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: `.${styles.videoContainer}`,
+        start: "25% bottom",
+        once: true,
+      },
+    });
+
+    tl.from(videoContainer.current!.children, {
+      y: "-10%",
+      duration: 2.5,
+      stagger: 0.1,
+      ease: "elastic",
+    }).from(
+      videoContainer.current!.children,
+      {
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.1,
+        ease: "power2.out",
+      },
+      "<",
+    );
+
+    gsap.to(videoContainer.current, {
+      opacity: 1,
+      duration: 2,
+      scrollTrigger: {
+        trigger: `.${styles.videoContainer}`,
+        start: "25% bottom",
+        once: true,
+      },
+    });
+  }, []);
 
   return (
     <section className={styles.sectionContainer}>
@@ -55,17 +85,19 @@ export const InstagramSection = () => {
         >
           <ArrowSVG className={styles.arrow} />
         </button>
+        <div ref={track} className={styles.track}></div>
         <div ref={videoContainer} className={styles.videoContainer}>
           {MOCKDATA.map((link, index) => {
             return (
-              <video
-                key={index}
-                className={clsx(
-                  { [styles.activeVideo]: index === activeIndex },
-                  styles.video,
-                )}
-                src={link}
-              />
+              <div key={index} className={styles.videoScaleWrapper}>
+                <video
+                  className={clsx(
+                    { [styles.activeVideo]: index === activeIndex },
+                    styles.video,
+                  )}
+                  src={link}
+                />
+              </div>
             );
           })}
         </div>

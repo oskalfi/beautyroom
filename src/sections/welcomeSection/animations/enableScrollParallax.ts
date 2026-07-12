db8debd0849878b156b4f69d0a5dc94f538ac9f7
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-07-12 23:39:40 +0300

    fix-test4: parallax scroll on phone

--- src/sections/welcomeSection/animations/enableScrollParallax.ts
+++ src/sections/welcomeSection/animations/enableScrollParallax.ts
@@ -5,12 +5,14 @@ export function enableScrollParallax(backgroundImageClass: string): void {
   gsap.registerPlugin(ScrollTrigger);
   ScrollTrigger.normalizeScroll(true);
   gsap.to(backgroundImageClass, {
-    y: 250,
+    yPercent: 80,
+    ease: "none",
     scrollTrigger: {
       trigger: backgroundImageClass,
       start: "top top",
       end: "bottom top",
-      scrub: 1,
+      scrub: true,
+      invalidateOnRefresh: true,
     },
   });
 }

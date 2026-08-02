d1c5a8f29d20a62704656e295d35513005b425fe
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-08-02 10:43:25 +0300

    check

--- src/sections/welcomeSection/animations/enableScrollParallax.ts
+++ src/sections/welcomeSection/animations/enableScrollParallax.ts
@@ -5,15 +5,41 @@ export function enableScrollParallax(backgroundImageClass: string): void {
   gsap.registerPlugin(ScrollTrigger);
   const mm = gsap.matchMedia();
 
-  mm.add("(min-width: 768px)", () => {
+  mm.add("(min-width: 769px)", () => {
     gsap.to(backgroundImageClass, {
       yPercent: 32,
+
       ease: "none",
+
       scrollTrigger: {
         trigger: backgroundImageClass,
+
         start: "top top",
+
         end: "bottom top",
+
         scrub: true,
+
+        invalidateOnRefresh: true,
+      },
+    });
+  });
+
+  mm.add("(max-width: 768px)", () => {
+    gsap.to(backgroundImageClass, {
+      yPercent: 8,
+
+      ease: "none",
+
+      scrollTrigger: {
+        trigger: backgroundImageClass,
+
+        start: "top top",
+
+        end: "bottom top",
+
+        scrub: true,
+
         invalidateOnRefresh: true,
       },
     });

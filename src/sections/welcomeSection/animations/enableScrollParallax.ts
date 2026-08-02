c88cacbf6eeca950bf3ae83ecdb8fe931d39d7ae
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-08-02 10:40:21 +0300

    check

--- src/sections/welcomeSection/animations/enableScrollParallax.ts
+++ src/sections/welcomeSection/animations/enableScrollParallax.ts
@@ -3,15 +3,19 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export function enableScrollParallax(backgroundImageClass: string): void {
   gsap.registerPlugin(ScrollTrigger);
-  gsap.to(backgroundImageClass, {
-    yPercent: 32,
-    ease: "none",
-    scrollTrigger: {
-      trigger: backgroundImageClass,
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-      invalidateOnRefresh: true,
-    },
+  const mm = gsap.matchMedia();
+
+  mm.add("(min-width: 768px)", () => {
+    gsap.to(backgroundImageClass, {
+      yPercent: 32,
+      ease: "none",
+      scrollTrigger: {
+        trigger: backgroundImageClass,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        invalidateOnRefresh: true,
+      },
+    });
   });
 }

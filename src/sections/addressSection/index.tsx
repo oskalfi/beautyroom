d697bfdf76b35111509c585f374f3e83b6c4bba6
Adam Stadnikov <stadnikov.adam@gmail.com>
2026-08-24 15:07:55 +0300

    animated logo

--- src/sections/addressSection/index.tsx
+++ src/sections/addressSection/index.tsx
@@ -10,24 +10,30 @@ export const AddressSection = () => {
     <section className={styles.section}>
       <h2 className={styles.heading}>Расположение</h2>
 
-      <div className={styles.contentWrapper}>
-        <Image
-          className={styles.logo}
-          src="/logoKY.svg"
-          alt="Logo"
-          width={230}
-          height={230}
-        />
+      <a
+        href="https://waze.com/ul?q=Jerusalem%20Blvd%2033%2C%20Tel%20Aviv-Yafo&navigate=yes"
+        className={styles.contentWrapper}
+      >
+        <div className={styles.plateWrapper}>
+          <div className={styles.plate}>
+            <div className={styles.plateContent}>
+              <Image
+                className={styles.logo}
+                src="/logoKY.svg"
+                alt="Logo"
+                width={230}
+                height={230}
+              />
+            </div>
+            <div className={styles.stickerBack}></div>
+          </div>
+        </div>
+
         <address className={styles.address}>
-          <a
-            className={styles.link}
-            href="https://waze.com/ul?q=Jerusalem%20Blvd%2033%2C%20Tel%20Aviv-Yafo&navigate=yes"
-          >
-            <img src="/waze.svg" alt="Waze icon" className={styles.wazeIcon} />
-            Jerusalem Blvd 33, Tel Aviv-Yafo
-          </a>
+          <img src="/waze.svg" alt="Waze icon" className={styles.wazeIcon} />
+          Jerusalem Blvd 33, Tel Aviv-Yafo
         </address>
-      </div>
+      </a>
       <img className={styles.map} src="/map.png" alt="Map" />
     </section>
   );
